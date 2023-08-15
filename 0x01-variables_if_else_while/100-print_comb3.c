de5f50a23fbb3a17459161f70570c6a67729fbb9
nada778 <nadadesoky68@gmail.com>
2023-08-14 19:36:48 -0700

    edit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,6 +3,7 @@
 /**
  * main - prints all possible different combinations of two digits
  * Return: ALways 0 (Success)
+ * program that prints all possible different combinations
  */
 int main(void)
 {
