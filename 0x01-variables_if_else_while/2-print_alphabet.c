e71995d12b80d665f7ff28e8bd0b72defe3c6270
nada778 <nadadesoky68@gmail.com>
2023-08-14 18:26:16 -0700

    that was fun

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int a;
+
+	for (a = 0; a < 26; i++)
+	{
+		putchar(alp[a]);
+	}
+	putchar('\n');
+	return (0);
+}
+
