b3a6b41ac732a275b5009447b81afbf010152055
nada778 <nadadesoky68@gmail.com>
2023-08-15 17:58:24 -0700

    as

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * nada hesham
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  * followed by a new line
  */
+
+/* nada */
+
 void print_alphabet_x10(void)
 {
 	char ch;
